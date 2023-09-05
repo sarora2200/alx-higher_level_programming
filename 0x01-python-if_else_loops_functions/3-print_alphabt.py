419958173301092ac7bc4183da6f1dd38434ce13
sarora2200 <sarora2200@gmail.com>
2023-09-05 11:20:32 -0700

    done

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if letter != 113 and letter != 120:
-        print("{:c}".format(letter), end="")
+    if letter == 113 or letter == 101:
+        continue
+    print("{:c}".format(letter), end="")
