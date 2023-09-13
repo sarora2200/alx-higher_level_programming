fffd7dd43afd3798aad53c1ac128df0ab3bac4ad
sarora2200 <sarora2200@gmail.com>
2023-09-13 12:20:08 -0700

    done

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -20,14 +20,14 @@ Py_ssize_t size, i;
 PyObject *element;
 size = PyList_Size(p);
 printf("[*] Python list info\n[*] Size of the Python List
-		= %ld\n[*] Allocated = %ld\n", size, ((PyListObject *)p)->allocated);
+= %ld\n[*] Allocated = %ld\n", size, ((PyListObject *)p)->allocated);
 for (i = 0; i < size; i++)
 {
 element = PyList_GetItem(p, i);
 printf("Element %ld: %s\n", i, Py_TYPE(element)->tp_name);
 if (PyBytes_Check(element))
 printf("[.] bytes object info\n  size: %ld\n  trying string: %s\n",
-		PyBytes_Size(element), PyBytes_AsString(element));
+PyBytes_Size(element), PyBytes_AsString(element));
 }
 }
 
