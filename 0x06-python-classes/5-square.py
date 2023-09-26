8039cdd2859899d20ba7442a4b794c59e60be602
sarora2200 <sarora2200@gmail.com>
2023-09-26 14:57:08 -0700

    done

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -13,27 +13,27 @@ class Square:
         """
         self.size = size
 
-        @property
-        def size(self):
-            """Get/set the curret size of the square."""
-            return (self.__size)
+    @property
+    def size(self):
+        """Get/set the curret size of the square."""
+        return (self.__size)
 
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int)
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        def area(self):
-            """returns the current square area"""
-            return (self.__size * self.__size)
+    def area(self):
+        """returns the current square area"""
+        return (self.__size * self.__size)
 
-        def my_print(self):
-            """prints in stdout the square with the character #"""
-            for i in range(0, self.__size):
-                [print("#", end="") for j in range(self.__size)]
-                print("")
-            if self.__size == 0:
-                print("")
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
