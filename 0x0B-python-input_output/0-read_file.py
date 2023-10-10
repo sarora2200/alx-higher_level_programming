83ba84034a0b0d0d9f20e1189a73b63190b8d999
sarora2200 <sarora2200@gmail.com>
2023-10-10 08:55:42 -0700

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+0-read_file module
+"""
+
 
 def read_file(filename=""):
+    """
+    read_file - reads a text file (UTP8) and prints it to stdout
+    Args:
+        filename: name of the file
+    """
     with open(filename, 'r') as f:
         for i in f:
             print(i, end="")
