a9cd7f0503a6ad7b247140fbf0dc2f55abdbb980
sarora2200 <sarora2200@gmail.com>
2023-11-15 10:06:40 -0800

    done

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
 (
 	PRIMARY KEY(`id`),
 	`id` INT NOT NULL AUTO_INCREMENT,
