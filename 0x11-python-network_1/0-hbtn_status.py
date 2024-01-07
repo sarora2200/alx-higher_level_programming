83ffdf7d1089b9baf379c509ffb3beac8afdd7cf
sarora2200 <sarora2200@gmail.com>
2024-01-06 22:52:13 -0800

    done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" script that fetches https://alx-intranet.hbtn.io/status """
-import urllib.request
+""" script that fetches https://alx-intranet.hbtn.io/status"""
+from urllib import request
 
 
 if __name__ == "__main__":
